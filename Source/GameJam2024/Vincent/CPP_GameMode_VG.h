a0bbeb3a58808563262e17310e7dcd38facbb4a7
Nem <vincent.r.greene@gmail.com>
2024-01-28 10:03:22 +0000

    starting game loop

---
+++ Source/GameJam2024/Vincent/CPP_GameMode_VG.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_GameMode_VG.generated.h"
+
+/**
+ * 
+ */
+
+USTRUCT(BlueprintType)
+struct FSDayData
+{
+    GENERATED_BODY()
+
+public:
+    // Declare variables
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 Index;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString Title;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString Level;
+};
+
+
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FInteracting);
+
+UCLASS()
+class GAMEJAM2024_API ACPP_GameMode_VG : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TArray<FSDayData> DayList = { 
+        {0, FString("Day 01"), FString("L_Sandbox_VG")},
+        {1, FString("Day 02"), FString("L_Workshop_VG")},
+    };
+	
+};
